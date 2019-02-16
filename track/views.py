489c3466a99a95f125308ec3628fdb0c40e709a9
Will Keeling <will@zifferent.com>
2019-02-16 21:43:57 +0000

    Delegate conversion to model

--- track/views.py
+++ track/views.py
@@ -1,5 +1,3 @@
-import operator
-
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.generic.list import ListView
@@ -11,18 +9,10 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-OPERATOR = {
-    'kg': operator.truediv,
-    'stone': operator.mul
-}
-
-
 def chart(request):
     if request.method == 'GET':
         weight_data = {}
-        requested_unit = request.GET.get('unit')
-        if requested_unit not in OPERATOR:
-            requested_unit = 'kg'
+        unit = request.GET.get('unit')
 
         for record in WeightRecord.objects.all():
             name = '{} {}'.format(record.person.first_name, record.person.last_name)
@@ -30,12 +20,7 @@ def chart(request):
             weight_data[name] = []
 
             for measurement in record.measurements.all():
-                weight = measurement.weight
-
-                if measurement.unit != requested_unit:
-                    weight = round(OPERATOR[requested_unit](weight, 0.157473), ndigits=1)
-
-                weight_data[name].append({'x': measurement.created, 'y': weight})
+                weight_data[name].append({'x': measurement.created, 'y': measurement.weight_as(unit)})
 
         return JsonResponse(weight_data)
 
