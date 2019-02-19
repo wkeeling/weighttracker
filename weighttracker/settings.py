107dfac0013e48eb48360292c93594653af5107b
Will Keeling <will@zifferent.com>
2019-02-19 14:25:19 +0000

    Add authentication backend

--- weighttracker/settings.py
+++ weighttracker/settings.py
@@ -88,6 +88,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
