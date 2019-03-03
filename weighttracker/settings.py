c62914972e9080cc6da1e79a29f29a55bb3004ba
Will Keeling <will@zifferent.com>
2019-03-03 09:11:29 +0000

    Templates should be relative to BASE_DIR

--- weighttracker/settings.py
+++ weighttracker/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'weighttracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
